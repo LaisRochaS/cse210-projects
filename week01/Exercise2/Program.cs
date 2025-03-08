0177ecd820bc8822a1621002608272d50af559bc
LaisRochaS <laismaria713@gmail.com>
2025-03-07 22:14:31 -0500

    projects

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -2,8 +2,64 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grade percentage: ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+
+        string letter = "";
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine($"Congratulations! You passed the course with a grade of {letter}.");
+        }
+        else
+        {
+            Console.WriteLine($"Sorry, you did not pass. You received a grade of {letter}. Try harder next time!");
+        }
+        char sign = ' '; 
+
+        if (letter != "F") 
+        {
+            int lastDigit = gradePercentage % 10;
+
+            if (lastDigit >= 7)
+            {
+                sign = '+';
+            }
+            else if (lastDigit <= 2)
+            {
+                sign = '-';
+            }
+        }
+        if (sign != ' ')
+        {
+            Console.WriteLine($"Your final grade is {letter}{sign}.");
+        }
+        else
+        {
+            Console.WriteLine($"Your final grade is {letter}.");
+        }
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,66 @@
 using System;
-
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>(); 
+        int inputNumber = -1;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        while (inputNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            inputNumber = int.Parse(Console.ReadLine());
+
+            if (inputNumber != 0)
+            {
+                numbers.Add(inputNumber);
+            }
+        }
+
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        double average = sum / (double)numbers.Count;
+        int largestNumber = numbers[0];
+        foreach (int num in numbers)
+        {
+            if (num > largestNumber)
+            {
+                largestNumber = num;
+            }
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestNumber}");
+
+        int smallestPositive = int.MaxValue;
+        foreach (int num in numbers)
+        {
+            if (num > 0 && num < smallestPositive)
+            {
+                smallestPositive = num;
+            }
+        }
+        if (smallestPositive == int.MaxValue)
+        {
+            Console.WriteLine("There are no positive numbers.");
+        }
+        else
+        {
+            Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+        }
+
+        numbers.Sort(); 
+        Console.WriteLine("The sorted list is:");
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
     }
 }
